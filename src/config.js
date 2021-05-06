458b1c8e3549e9e3457e700924ba699322de6f05
isamu-isozaki <isamu.website@gmail.com>
2021-05-06 14:26:21 -0400

    Modified env variables

--- src/config.js
+++ src/config.js
@@ -4,14 +4,6 @@
  */
 module.exports = {
   APP_PORT: process.env.APP_PORT,
-  MILTER_PORT: process.env.MILTER_PORT,
-  KIARA_API_URL: process.env.KIARA_API_URL,
-  DB_URI: process.env.DB_URI,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  X_RAPIDAPI_HOST: process.env.X_RAPIDAPI_HOST,
-  X_RAPIDAPI_KEY: process.env.X_RAPIDAPI_KEY,
   API_URL: process.env.API_URL,
   DOMAIN_URL: process.env.DOMAIN_URL,
-  MAIL_HOST: process.env.MAIL_HOST,
 };
